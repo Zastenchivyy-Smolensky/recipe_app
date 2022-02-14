d8fb72585ce8816ff55b2dfca64c75c2b1173de3
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-02-14 12:36:29 +0900

    finish application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @users = User.all
   end
@@ -9,13 +10,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path(current_user), alert: "不正なアクセスです"
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    if @user.save
-      redirect_to user_path(@user), notice:"編集をしました"
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render :edit
     end
   end
 
